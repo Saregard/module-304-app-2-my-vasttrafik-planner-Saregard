a1b4c80a5d45df4ccb773b4ff0ec273fb4fbabbb
Saregard <77791326+Saregard@users.noreply.github.com>
2021-06-28 21:32:54 +0200

    App creation

--- app/src/main/java/com/example/simpleplanner/activities/MainActivity.kt
+++ app/src/main/java/com/example/simpleplanner/activities/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.simpleplanner
+package com.example.simpleplanner.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpleplanner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = getString(R.string.nearby_stops)
     }
 }
---
+++ app/src/main/java/com/example/simpleplanner/repository/Endpoint.kt
@@ -0,0 +1,4 @@
+package com.example.simpleplanner.repository
+
+interface Endpoint {
+}
