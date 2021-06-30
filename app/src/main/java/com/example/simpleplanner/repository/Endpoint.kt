34599646cec14fadd89ee4ab16a3b2a6e15d6745
Saregard <77791326+Saregard@users.noreply.github.com>
2021-06-30 17:11:48 +0200

    Update Endpoint

--- app/src/main/java/com/example/simpleplanner/repository/Endpoint.kt
+++ app/src/main/java/com/example/simpleplanner/repository/Endpoint.kt
@@ -1,5 +1,6 @@
 package com.example.simpleplanner.repository
 
+import com.example.simpleplanner.models.BusData
 import com.example.simpleplanner.models.StopData
 import com.example.simpleplanner.util.Credential
 import retrofit2.Call
@@ -9,11 +10,20 @@ import retrofit2.http.Query
 
 interface Endpoint {
 
-    @Headers("Authorization: ${Credential.VASTRAFIK_KEY}")
+    @Headers("Authorization: ${Credential.VASTTRAFIK_KEY}")
     @GET("location.nearbystops")
     fun getNearbyStops(
         @Query("originCoordLat") latitude: Double,
         @Query("originCoordLong") longitude: Double,
         @Query("format") format: String = "json"
     ): Call<StopData>
+
+    @Headers("Authorization: ${Credential.VASTTRAFIK_KEY}")
+    @GET("departureBoard")
+    fun getDepartureTimesFromStop(
+        @Query("id") stopId: Long,
+        @Query("date") date: String,
+        @Query("time") time: String,
+        @Query("format") format: String = "json"
+    ): Call<BusData>
 }
