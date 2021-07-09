3909f0d51017fe78c6738369ef4e1bb55a94c5c1
Saregard <77791326+Saregard@users.noreply.github.com>
2021-07-09 22:19:26 +0200

    Added funcitons
    Expand and collapse function added,
    
    time left added

--- app/src/main/java/com/example/simpleplanner/adapter/DepartureRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/simpleplanner/adapter/DepartureRecyclerViewAdapter.kt
@@ -3,12 +3,17 @@ package com.example.simpleplanner.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.os.Build
+import android.transition.TransitionManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simpleplanner.R
 import com.example.simpleplanner.databinding.LayoutRecyclerViewDepartureBinding
 import com.example.simpleplanner.models.Departure
+import java.time.LocalTime
 
 class DepartureRecyclerViewAdapter(
     private val listOfDepartures: List<Departure>,
@@ -18,6 +23,8 @@ class DepartureRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder.create(parent)
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(listOfDepartures[position], context)
     }
@@ -27,11 +34,22 @@ class DepartureRecyclerViewAdapter(
     class ViewHolder(private val binding: LayoutRecyclerViewDepartureBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        @RequiresApi(Build.VERSION_CODES.O)
         @SuppressLint("Range", "UseCompatLoadingForDrawables")
         fun bind(departure: Departure, context: Context) {
+            val currentTime = (LocalTime.now().toSecondOfDay() / 60)
+            val hour = departure.time.substring(0..1).toInt()
+            val minute = departure.time.substring(3..4).toInt()
+            val time = (hour * 60) + minute
+
+            val timeLeft = time - currentTime
+
+            binding.timeLeftTextView.text = timeLeft.toString() + " min"
+            binding.leftFrame.setBackgroundColor(Color.parseColor(departure.bgColor))
             binding.recyclerViewDepartureTextViewDirection.text = departure.direction
             binding.recyclerViewTextViewDepartureTime.text = departure.time
-            binding.recyclerViewTextViewTrack.text = context.getString(R.string.platform, departure.track)
+            binding.recyclerViewTextViewTrack.text =
+                context.getString(R.string.platform, departure.track)
             binding.recyclerViewTextViewLineNumber.apply {
                 text = departure.sname
                 setTextColor(Color.parseColor(departure.fgColor))
@@ -54,7 +72,23 @@ class DepartureRecyclerViewAdapter(
                 )
             )
 
+            binding.expandButton.setOnClickListener {
+                binding.expandButton.visibility = View.GONE
+                binding.collapseButton.visibility = View.VISIBLE
+                TransitionManager.beginDelayedTransition(binding.departureCardView)
+                binding.recyclerViewTextViewTrack.visibility = View.VISIBLE
+                binding.recyclerViewTextViewDepartureTime.visibility = View.VISIBLE
+            }
+
+            binding.collapseButton.setOnClickListener {
+                binding.expandButton.visibility = View.VISIBLE
+                binding.collapseButton.visibility = View.GONE
+                TransitionManager.beginDelayedTransition(binding.departureCardView)
+                binding.recyclerViewTextViewTrack.visibility = View.GONE
+                binding.recyclerViewTextViewDepartureTime.visibility = View.GONE
+            }
         }
+
 
         companion object {
             fun create(parent: ViewGroup): ViewHolder {
@@ -65,5 +99,4 @@ class DepartureRecyclerViewAdapter(
             }
         }
     }
-
 }
